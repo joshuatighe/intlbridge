59e782dcfcac2bb94e8046f8d363082a1711f4d0
Joshua Tighe <jtighe3@nd.edu>
2025-08-27 08:46:41 -0400

    add theme switcher

--- src/components/heading.tsx
+++ src/components/heading.tsx
@@ -10,7 +10,7 @@ const Heading = ({ title, description }: HeadingProps) => {
     <>
       <div className="px-8">
         <h2 className="text-3xl font-bold tracking-tight">{title}</h2>
-        {description && <p className="text-sm text-slate-500">{description}</p>}
+        {description && <p className="text-sm text-gray-500">{description}</p>}
       </div>
 
       <Separator />
