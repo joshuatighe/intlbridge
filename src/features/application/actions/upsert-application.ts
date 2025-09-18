82ce29958a6ba3d14691f6db707642d1ca15f189
Joshua Tighe <jtighe3@nd.edu>
2025-09-18 08:41:21 -0400

    add form validation with zod

--- src/features/application/actions/upsert-application.ts
+++ src/features/application/actions/upsert-application.ts
@@ -2,26 +2,36 @@
 
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import { z } from "zod";
 import { prisma } from "@/lib/prisma";
 import { applicationPath, applicationsPath } from "@/paths";
+
+const upsertApplicationSchema = z.object({
+  college: z.string(),
+  notes: z.string().min(1).max(1024),
+});
 
 export const upsertApplication = async (
   id: string | undefined,
   _actionState: { message: string },
   formData: FormData,
 ) => {
-  const data = {
-    college: formData.get("college") as string,
-    notes: formData.get("notes") as string,
-  };
+  try {
+    const data = upsertApplicationSchema.parse({
+      college: formData.get("college"),
+      notes: formData.get("notes"),
+    });
 
-  await prisma.application.upsert({
-    where: {
-      id: id || "",
-    },
-    update: data,
-    create: data,
-  });
+    await prisma.application.upsert({
+      where: {
+        id: id || "",
+      },
+      update: data,
+      create: data,
+    });
+  } catch (_error) {
+    return { message: "Something went wrong" };
+  }
 
   revalidatePath(applicationsPath());
 
